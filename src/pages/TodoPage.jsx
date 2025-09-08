df9edd947361dfde6f087617a15318dfc5d5a9a7
Tham <hang.ma.230142@student.fulbirght.edu.vn>
2025-09-08 19:10:31 +0700

    feat: add new pages for todo app

---
+++ src/pages/TodoPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoPage = () => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {/* M code component Todo list ở đây */}
+    </div>
+  );
+};
+
+export default TodoPage;
