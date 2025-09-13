48f2c6cd6e03f052fd3e1c3d8262d66c7fb0dd30
Tham <hang.ma.230142@student.fulbirght.edu.vn>
2025-09-13 13:54:57 +0700

    add new pages

--- src/layouts/Mainlayout.tsx
+++ src/layouts/Mainlayout.tsx
@@ -1,15 +1,38 @@
 import React from "react";
-import Header from "../components/Header";
-
-const MainLayout: React.FC = () => {
+import { Link, Outlet } from "react-router-dom";
+const Mainlayout: React.FC = () => {
   return (
-    <div>
-      <Header />
-      <main style={{ padding: "1rem" }}>
-        {/* Let do */}
+    <div style={{ display: "flex", height: "100vh" }}>
+      <nav
+        style={{
+          width: "220px",
+          background: "#ddb4ffff",
+          color: "white",
+          padding: "1rem",
+        }}
+      >
+        <h2>Student Life Manager</h2>
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          <li><Link to="/" style={{ color: "white" }}>Home</Link></li>
+          <li><Link to="/product" style={{ color: "white" }}>Product</Link></li>
+          <li><Link to="/calendar" style={{ color: "white" }}>Calendar</Link></li>
+          <li><Link to="/pomodoro" style={{ color: "white" }}>Pomodoro</Link></li>
+          <li><Link to="/time-management" style={{ color: "white" }}>Time Management</Link></li>
+          <li><Link to="/categories" style={{ color: "white" }}>Categories</Link></li>
+          <li><Link to="/collaboration" style={{ color: "white" }}>Collaboration</Link></li>
+          <li><Link to="/habit-tracking" style={{ color: "white" }}>Habit Tracking</Link></li>
+          <li><Link to="/statistics" style={{ color: "white" }}>Statistics</Link></li>
+          <li><Link to="/settings" style={{ color: "white" }}>Settings</Link></li>
+          <li><Link to="/help" style={{ color: "white" }}>Help Center</Link></li>
+          <li><Link to="/about" style={{ color: "white" }}>About Us</Link></li>
+        </ul>
+      </nav>
+
+      {/* Main content */}
+      <main style={{ flex: 1, padding: "1rem" }}>
+        <Outlet />
       </main>
     </div>
   );
 };
-
-export default MainLayout;
+export default Mainlayout;
--- src/main.tsx
+++ src/main.tsx
@@ -5,34 +5,38 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Calendar from "./pages/Calendar";
-import TaskDetails from "./pages/TaskDetails";
-import Setting from "./pages/Setting";
-import HelpCenter from "./pages/HelpCenter";
-import AboutUs from "./pages/AboutUs";
 import Pomodoro from "./pages/Pomodoro";
 import TimeManagement from "./pages/TimeManagement";
 import Categories from "./pages/Categories";
+import Setting from "./pages/Setting";
+import HelpCenter from "./pages/HelpCenter";
+import AboutUs from "./pages/AboutUs";
+import Collaborationpage from "./pages/Collaborationpage";
+import Habittrackingpage from "./pages/Habittrackingpage";
 import Stasticspage from "./pages/Stasticspage";
-import Collaboration from "./pages/Collaborationpage";
-import Habbittracking from "./pages/Habittrackingpage";
+
+import Mainlayout from "./layouts/Mainlayout";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/task-details" element={<TaskDetails />} />
-        <Route path="/setting" element={<Setting />} />
-        <Route path="/help-center" element={<HelpCenter />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/pomodoro" element={<Pomodoro />} />
-        <Route path="/time-management" element={<TimeManagement />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/statistics" element={<Stasticspage />} />
-        <Route path="/collaboration" element={<Collaboration />} />
-        <Route path="/habbit-tracking" element={<Habbittracking />} />
+        {/* Layout cha */}
+        <Route path="/" element={<Mainlayout />}>
+          {/* Các page con */}
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="pomodoro" element={<Pomodoro />} />
+          <Route path="time-management" element={<TimeManagement />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="collaboration" element={<Collaborationpage />} />
+          <Route path="habit-tracking" element={<Habittrackingpage />} />
+          <Route path="statistics" element={<Stasticspage />} />
+          <Route path="settings" element={<Setting />} />
+          <Route path="help" element={<HelpCenter />} />
+          <Route path="about" element={<AboutUs />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
